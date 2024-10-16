68013fd6d956989f183edd603118ac65f2a48843
23jy0104 <23jy0104@jec.ac.jp>
2024-10-16 15:29:16 +0900

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HellowWorld{
   public static void main(String[]args){
     System.out.println("Hello World");
+    System.out.println("Hello World");
+    System.out.println("日本電子専門学校");
   }
 }

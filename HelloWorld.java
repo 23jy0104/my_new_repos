2b119df0ba40fcef7e8bd56b844e9cf42201a6d8
'itou' <'23jy0104@jec.ac.jp'>
2024-10-23 16:34:50 +0900

    ハロー

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ public class HellowWorld{
     System.out.println("Hello World");
     System.out.println("Hello World");
     System.out.println("日本電子専門学校");
+    System.out.println("日本電子専門学校");
   }
 }
